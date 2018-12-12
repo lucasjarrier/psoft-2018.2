c82d50ea67a09e77d4fe90b35f66060d363c95ad
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-12 10:44:45 -0300

    Update Cliente.java

--- src/main/java/com/cccpharma/models/user/Cliente.java
+++ src/main/java/com/cccpharma/models/user/Cliente.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
