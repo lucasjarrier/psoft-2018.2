4dad614f4a2a95688e3844c2f03c41965d50fb1a
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-15 10:38:50 -0300

    Update ClientController.java

--- src/main/java/com/cccpharma/controllers/ClientController.java
+++ src/main/java/com/cccpharma/controllers/ClientController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cccpharma.models.user.Cliente;
 import com.cccpharma.services.ClientService;
 
-@CrossOrigin("*")
+@CrossOrigin(value ="*")
 @RestController
 public class ClientController {
 
