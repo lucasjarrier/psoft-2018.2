b26092101b2600fbd13c6a31ac0d28ea70af4df6
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-16 21:29:15 -0300

    Update RegistroController.java

--- src/main/java/com/cccpharma/controllers/RegistroController.java
+++ src/main/java/com/cccpharma/controllers/RegistroController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.cccpharma.models.Produto;
 import com.cccpharma.models.Registro;
 import com.cccpharma.services.RegistroService;
 
+@CrossOrigin(value = "*")
 @RestController
 public class RegistroController {
 
